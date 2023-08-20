0e0d06d4423678898d5461634759bd3be8b713b5
Elcris14 <cristianpalazzo14@gmail.com>
2023-08-20 07:25:10 -0300

    Version 1.0

---
+++ ProyectoWeb/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.blog, name="blog"),
+    path('categoria/<categoria_id>/', views.categoria, name="categoria"),
+
+]
+
