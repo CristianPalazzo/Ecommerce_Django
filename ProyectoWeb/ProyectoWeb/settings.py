63361340e86fce3b73f649c2780b554f0d76948b
Elcris14 <cristianpalazzo14@gmail.com>
2023-08-20 07:36:20 -0300

    Version 1.0

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="maildedjango@gmail.com"
-EMAIL_HOST_PASSWORD="ioowrprdrxbyhinb"
+EMAIL_HOST_USER="Aqui va el email destinatario"
+EMAIL_HOST_PASSWORD="Aqui va la contraseña de su email"
 
 MESSAGE_TAGS={
     
